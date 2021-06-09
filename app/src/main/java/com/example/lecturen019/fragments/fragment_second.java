ce4160a00f959593054d368e217e028b3e98e426
unknown <asmak9060@gamil.com>
2021-06-09 12:41:31 +0500

    2nd commit

---
+++ app/src/main/java/com/example/lecturen019/fragments/fragment_second.java
@@ -0,0 +1,25 @@
+package com.example.lecturen019.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lecturen019.R;
+public class fragment_second extends Fragment {
+    public fragment_second() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_second, container, false);
+
+
+        return view;
+    }
+}
